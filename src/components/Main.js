59cb3c4fcf1dc4c863d75148df0d077fdf2682ab
shenyuanchen <shenyuanchen@vchangyi.com>
2016-06-21 00:37:12 +0800

    图片css样式

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,18 +11,40 @@ imageDatas = (function(imagesDataArr) {
 	for(var i =0, len = imagesDataArr.length; i<len; i++) {
 		var singleImageData = imagesDataArr[i];
 		singleImageData.imgUrl = require('../images/'+ singleImageData.fileName);
-		imagesDataArr[i] = singleImageData.imgUrl;
+		imagesDataArr[i] = singleImageData;
 	}
 	return imagesDataArr;
 })(imageDatas);
 
-console.log(imageDatas)
+class ImgFigure extends React.Component {
+	render() {
+		return (
+			<figure className="figure-imgWarp">
+				<img className="figure-img" src={this.props.data.imgUrl} alt={this.props.data.title}/>
+				<figcaption className="figure-title">
+					<h2>{this.props.data.desc}</h2>
+				</figcaption>
+			</figure>
+		)
+	}
+}
+
 
 class AppComponent extends React.Component {
   render() {
+	
+	var figureImgs = [];
+
+	imageDatas.forEach(function(item) {
+		figureImgs.push(<ImgFigure key={item.id} data={item} />);
+	});
+	
+
     return (
       <section className="stage">
-      	<section className="images-warp"></section>
+      	<section className="images-warp">
+			{figureImgs}
+      	</section>
       	<nav className="controller-nav"></nav>
       </section>
     );
